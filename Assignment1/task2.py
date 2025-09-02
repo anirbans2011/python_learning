1845cd7030f69d301493affa17c73d097bb32ce1
anirbans2011 <anirbansantra2011@gmail.com>
2025-09-02 12:19:12 +0530

    Added python code for assignment1 task2.py

---
+++ Assignment1/task2.py
@@ -0,0 +1,4 @@
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+full_name = firstname + " " + lastname
+print("Hello, " + full_name + "! Welcome to the Python program")
