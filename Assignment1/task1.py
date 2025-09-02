f102c653842d65db0b60427cee372dd52ee20ea7
anirbans2011 <anirbansantra2011@gmail.com>
2025-09-02 11:34:16 +0530

    added the python code for Assignment1 task1

---
+++ Assignment1/task1.py
@@ -0,0 +1,10 @@
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+addition = a + b
+substraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition: ",addition)
+print("Substraction: ",substraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
